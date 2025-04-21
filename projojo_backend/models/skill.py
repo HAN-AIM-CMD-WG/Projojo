0e00dfb7392a7c8287ef3ae48c2eec52af3d1fb3
PedrovD <pedro_vandouveren@hotmail.com>
2025-04-21 12:11:14 +0200

    Refactor Skill model to fix datetime handling.
     Introduced a custom JSON encoder for datetime formatting

--- projojo_backend/models/skill.py
+++ projojo_backend/models/skill.py
@@ -1,15 +1,22 @@
-from pydantic import BaseModel
-from typing import List, Optional
+from pydantic import BaseModel, Field
+from typing import Annotated
 from datetime import datetime
 
 class Skill(BaseModel):
     id: str
     name: str
     is_pending: bool
-    created_at: datetime
+    created_at: Annotated[datetime, Field(
+        examples=["2025-04-21T10:02:58"]
+    )]
+
     
     class Config:
         from_attributes = True
+        json_encoders = {
+            datetime: lambda dt: dt.strftime("%Y-%m-%dT%H:%M:%S")
+        }
+
 
 class StudentSkill(BaseModel):
     student_id: str
