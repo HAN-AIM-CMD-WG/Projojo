f630c4277d06aa93d1576d382b77e3e05aa440c0
Robert Holwerda <robert.holwerda@han.nl>
2025-04-24 12:53:21 +0200

    create-typedb script no longer pulls :latest typedb image

--- create_typedb.sh
+++ create_typedb.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
-# Stop and remove the existing TypeDB container and image
-docker stop typedb || true
-docker rm typedb --force || true
-docker rmi typedb/typedb:latest --force || true
+# Stop and remove the existing TypeDB container
+echo -n "Stopping container: "
+docker container stop typedb || true
+echo -n "Removing container: "
+docker container remove typedb --force || true
 
 # Handle reset-volume argument
 if [[ "$*" == *reset-volume* ]]; then
-    docker volume rm typedb-data --force || true
+    echo -n "Removing volume: "
+    docker volume remove typedb-data --force || true
 fi
 
 # Check if the volume exists, create if not
 docker volume inspect typedb-data > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-    echo "Creating volume 'typedb-data'..."
+    echo -n "Creating volume:"
     docker volume create typedb-data
 fi
 
+# download the correct image
+echo -n "Downloading image: "
+docker image pull --quiet typedb/typedb:3.1.0
 # Create the new container with the (new) volume
-docker create --name typedb -v typedb-data:/opt/typedb-all-linux-x86_64/server/data -p 1729:1729 --platform linux/amd64 typedb/typedb:latest
+echo -n "Creating container: "
+docker container create --name typedb \
+                        --restart unless-stopped \
+                        --volume typedb-data:/opt/typedb-all-linux-x86_64/server/data \
+                        --publish 1729:1729 \
+                        --platform linux/amd64 \
+                        typedb/typedb:3.1.0
+# start the db
+echo -n "Starting container: "
+docker container start typedb
