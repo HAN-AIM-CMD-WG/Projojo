99415e56f054aba487013a3ba1d79205527cb9ed
Stijn Appeldoorn <102795120+stijnapp@users.noreply.github.com>
2025-05-03 17:33:34 +0200

    removed comment

--- projojo_backend/main.py
+++ projojo_backend/main.py
@@ -72,5 +72,4 @@ async def typedb_status(db=Depends(get_db)):
         }
 
 if __name__ == "__main__":
-    # uvicorn.run(app, host="0.0.0.0", port=8000)
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
