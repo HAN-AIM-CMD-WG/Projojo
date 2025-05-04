c4dbec7ca3bb10741a1f9fc091822ab713b58916
PedrovD <pedro_vandouveren@hotmail.com>
2025-05-05 01:37:37 +0200

    Implement token-based authentication in AuthProvider
    Added logic to decode JWT tokens and set user authentication data based on the role obtained from the token.

--- frontend/src/components/AuthProvider.jsx
+++ frontend/src/components/AuthProvider.jsx
@@ -1,11 +1,41 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 // eslint-disable-next-line no-unused-vars
 import { getAuthorization } from '../services';
+import { jwtDecode } from "jwt-decode";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [authData, setAuthData] = useState({ type: "none", userId: null, businessId: null, isLoading: true });
+
+
+    useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        try {
+
+        const decoded = jwtDecode(token);
+        const { sub, role, business } = decoded;
+
+        if (role === "supervisor") {
+            setAuthData({ type: "supervisor", userId: sub, businessId: business, isLoading: false });
+        } else if (role === "student") {
+            setAuthData({ type: "student", userId: sub, businessId: null, isLoading: false });
+        } else if (role === "teacher") {
+            setAuthData({ type: "teacher", userId: sub, businessId: null, isLoading: false });
+        } else {
+            // fallback
+            setAuthData({ type: "none", userId: null, businessId: null, isLoading: false });
+        }
+        } catch (err) {
+        console.error("Invalid token:", err);
+        localStorage.removeItem("token");
+        setAuthData({ type: "none", userId: null, businessId: null, isLoading: false });
+        }
+    } else {
+        setAuthData({ type: "none", userId: null, businessId: null, isLoading: false });
+    }
+    }, []);
 
     return (
         <AuthContext.Provider value={{ authData, setAuthData }}>
