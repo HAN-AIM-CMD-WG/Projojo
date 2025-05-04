0f8171b5b4cf45c9f551956a90827130f8504687
PedrovD <pedro_vandouveren@hotmail.com>
2025-05-05 01:10:33 +0200

    Refactor BusinessCard props for readability and fix key reference
    Improved code readability by formatting the props passed to the BusinessCard component. Fixed an issue where the incorrect key `business_id` was used, replacing it with the correct `id` field.

--- frontend/src/components/BusinessProjectDashboard.jsx
+++ frontend/src/components/BusinessProjectDashboard.jsx
@@ -4,8 +4,16 @@ import ProjectDashboard from './ProjectDashboard';
 export default function BusinessProjectDashboard({ business, projects, topSkills, showDescription = false, showUpdateButton = false, isAlwaysExtended = false}) {
     return (
         <div className="bg-slate-100 rounded-lg overflow-hidden">
-            {console.log("projects", business)}
-            <BusinessCard name={business?.name} image={business?.image_path} location={business?.location} showUpdateButton={showUpdateButton} businessId={business?.business_id} showDescription={showDescription} description={business?.description} topSkills={topSkills} />
+            <BusinessCard 
+                name={business?.name} 
+                image={business?.image_path} 
+                location={business?.location} 
+                showUpdateButton={showUpdateButton} 
+                businessId={business?.id} 
+                showDescription={showDescription} 
+                description={business?.description} 
+                topSkills={topSkills} 
+            />
             {projects?.length > 0 && <ProjectDashboard projects={projects} isAlwaysExtended={isAlwaysExtended} />}
         </div>
     );
