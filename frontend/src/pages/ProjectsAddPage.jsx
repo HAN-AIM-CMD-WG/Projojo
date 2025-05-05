5af65dbe16535933cd5e61d416234bd3e0d73deb
PedrovD <pedro_vandouveren@hotmail.com>
2025-05-05 14:13:09 +0200

    reintroduced the ability to create projects on the frontend

--- frontend/src/pages/ProjectsAddPage.jsx
+++ frontend/src/pages/ProjectsAddPage.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AddProjectForm from "../components/AddProjectForm";
-import { /*createProject*/ } from "../services";
+import { createProject } from "../services";
 
 export default function ProjectsAddPage() {
     const navigate = useNavigate();
     const [serverErrorMessage, setServerErrorMessage] = useState('');
 
     const onSubmit = (data) => {
+        // Check if there's an error from the image upload
+        if (data.error) {
+            setServerErrorMessage(data.error);
+            return;
+        }
+        
         createProject(data)
             .then(newProjectId => {
                 navigate(`/projects/${newProjectId}`);
