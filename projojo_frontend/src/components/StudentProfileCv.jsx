91197cf8661c25153115d27790cb2b9df2c5f8c8
PedrovD <pedro_vandouveren@hotmail.com>
2025-05-10 00:33:36 +0200

    changed some styling to also house the lack of a CV and removed shallow chekcing of absense of CV
    in the previous form it would only care if the value has a string value, if it then did not find anything on the backend it would constantly show the /loading.gif. now it actually checks with a HEAD request and determines form there

--- projojo_frontend/src/components/StudentProfileCv.jsx
+++ projojo_frontend/src/components/StudentProfileCv.jsx
@@ -1,15 +1,37 @@
-import { FILE_BASE_URL } from "../services";
+import { PDF_BASE_URL } from "../services";
+import { useEffect, useState } from "react";
 import PdfPreview from "./PdfPreview";
 
+
 export default function StudentProfileCv({ cv }) {
+
+    const [exists, setExists] = useState(null);
+    
+    // Check if the CV exists by making a HEAD request to the URL
+    useEffect(() => {
+        if (!cv) {
+            setExists(false);
+            return;
+        }
+
+        const url = `${PDF_BASE_URL}${cv}`;
+
+        fetch(url, { method: "HEAD" })
+            .then(res => setExists(res.ok))
+            .catch(() => setExists(false));
+    }, [cv]);
+
     return (
         <div className="flex flex-col gap-4 w-full bg-gray-100 rounded-b-lg overflow-hidden">
-            {cv === null
-                ? <h2>Er is geen cv om weer te geven</h2>
-                : (
-                    <PdfPreview link={cv ? `${FILE_BASE_URL}${cv}` : "/loading.gif"} />
-                )
-            }
+            <div className="bg-gray-200 p-1 rounded-lg">
+            <h2 className="text-lg ms-1 mb-2 font-semibold">CV</h2>
+                {exists === false
+                    ? <h2>Er is geen cv om weer te geven</h2>
+                    : (
+                        <PdfPreview link={cv ? `${PDF_BASE_URL}${cv}` : "/loading.gif"} />
+                    )
+                }
+            </div>
         </div>
     )
 }
