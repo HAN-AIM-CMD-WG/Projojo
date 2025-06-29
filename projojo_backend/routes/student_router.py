e1e79fbcd944108380c9d2aa2cef1099b46e4083
evaketelaar <ewm.ketelaar@student.han.nl>
2025-06-29 17:20:52 +0200

    added endpoint

--- projojo_backend/routes/student_router.py
+++ projojo_backend/routes/student_router.py
@@ -1,12 +1,14 @@
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Path, Body, HTTPException
 
 from domain.repositories import SkillRepository, UserRepository
+
 skill_repo = SkillRepository()
 user_repo = UserRepository()
 
 from domain.models import StudentSkills
 
 router = APIRouter(prefix="/students", tags=["Student Endpoints"])
+
 
 @router.get("/")
 async def get_all_students():
@@ -25,7 +27,22 @@ async def get_student_skills(email: str = Path(..., description="Student email")
     student = user_repo.get_student_by_id(email)
     skills = skill_repo.get_student_skills(student.email)
 
-    return StudentSkills(
-        **student.model_dump(),
-        Skills=skills
-    )
+    return StudentSkills(**student.model_dump(), Skills=skills)
+
+
+@router.put("/{email}/skills")
+async def update_student_skills(
+    email: str = Path(..., description="Student email"),
+    skills: list[str] = Body(..., description="List of skillnames"),
+):
+    """
+    Update skills for a student
+    """
+    try:
+        skill_repo.update_student_skills(email, skills)
+        return {"message": "Skills succesvol bijgewerkt"}
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Er is iets misgegaan bij het bijwerken van de skills: {str(e)}",
+        )
